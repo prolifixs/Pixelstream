a7d42ac0c55ef83a566e10aa50854cfe17e7b693
prolifixs <prolifixs.pj@gmail.com>
2018-01-01 14:35:07 +0300

    Initial commit - git setup/connection and fire-base initialization

---
+++ app/src/main/java/com/prolifixs/pixelstream/MainHomeActivity.java
@@ -0,0 +1,16 @@
+package com.prolifixs.pixelstream;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Prolifixs on 1/1/2018.
+ */
+
+public class MainHomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
