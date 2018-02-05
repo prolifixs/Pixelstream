5fe07817d03b74aca32d32fae28fc426d82ba579
prolifixs <prolifixs.pj@gmail.com>
2018-02-05 12:16:51 +0300

    PERMISSIONS - Request permission to read and write data from phone memory.

--- app/src/main/java/com/prolifixs/pixelstream/Upload/MainUpload/UploadActivity.java
+++ app/src/main/java/com/prolifixs/pixelstream/Upload/MainUpload/UploadActivity.java
@@ -1,8 +1,12 @@
 package com.prolifixs.pixelstream.Upload.MainUpload;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +15,8 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.prolifixs.pixelstream.R;
 import com.prolifixs.pixelstream.Utils.BottomNavigationViewHelper;
+import com.prolifixs.pixelstream.Utils.Permissions;
+import com.prolifixs.pixelstream.Utils.SectionsPagerAdapter;
 
 /**
  * Created by Prolifixs on 1/3/2018.
@@ -18,7 +24,13 @@ import com.prolifixs.pixelstream.Utils.BottomNavigationViewHelper;
 
     public class UploadActivity extends AppCompatActivity {
     private static final String TAG = "UploadActivity";
+
+    //Constants
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
     private static final int ACTIVITY_NUM = 2;
+
+    //vars
+    private ViewPager mViewPager;
 
     private Context mContext = UploadActivity.this;
 
@@ -28,7 +40,70 @@ import com.prolifixs.pixelstream.Utils.BottomNavigationViewHelper;
         setContentView(R.layout.activity_main_upload);
         Log.d(TAG, "onCreate: Starting activity");
 
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+
+        }else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+        //setupBottomNavigationView();
+    }
+
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        //adapter.addFragment(new GalleryFragment);
+        //adapter.addFragment(new VideoFragment);
+        //mViewPager = (ViewPager)findViewById(R.id.container);
+       // mViewPager.setAdapter(adapter);
+
+        //TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
+        //tabLayout.setupWithViewPager(mViewPager);
+        //tabLayout.getTabAt(0).setText(getString(R.string.gallery));
+        //tabLayout.getTabAt(1).setText(getString(R.string.video));
+    }
+    /*
+    * Permissions verification ---------------------------------------
+    * */
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        ActivityCompat.requestPermissions(
+                UploadActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+    /*
+    * Checking an array of permission.
+    * */
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for(int i = 0; i< permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    * Checking a single permission if it has been verified.
+    * */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(UploadActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
     }
 
     /*
---
+++ app/src/main/java/com/prolifixs/pixelstream/Utils/Permissions.java
@@ -0,0 +1,16 @@
+package com.prolifixs.pixelstream.Utils;
+
+
+import android.Manifest;
+
+/**
+ * Created by Prolifixs on 2/4/2018.
+ */
+
+public class Permissions {
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
